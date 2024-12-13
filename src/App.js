b64e095943753e3fb1b065a78699ba7b0d004816
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2024-12-13 16:10:36 -0500

    Updated syntax and tsconfig
    Co-authored-by: Peyton <https://github.com/PalpaWalker>
    Co-authored-by: Mohammed <https://github.com/muhsin308>
    Co-authored-by: Muntasir <https://github.com/moha0988>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import Login from './frontend/LoginPage/Login';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <Login></Login>
     </div>
   );
 }
