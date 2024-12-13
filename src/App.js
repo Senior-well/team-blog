d7514fc81ceab759d198f50f32ef527e878fec0b
Yen Huynh Hoai <peterhuynh.contact@gmail.com>
2024-12-13 15:51:46 -0500

    Set up tsconfig pack Co-authored-by: Peyton Da Silva <https://github.com/PalpaWalker>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Login from './frontend/LoginPage/Login';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <Login></Login>
     </div>
   );
 }
